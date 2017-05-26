9bdcffe5d3af401ecb7293cb54c81833197f3f58
Ahmad N. Raja <ar@penneo.com>
2017-05-26 14:12:08 +0200

    Getting users of a customer

---
+++ cs/customer-users/README.md
@@ -0,0 +1,5 @@
+# Getting users for a customer
+
+## Requirements
+
+This example runs on Penneo .NET SDK version 1.4.0 and above.
---
+++ cs/customer-users/run.cs
@@ -0,0 +1,45 @@
+using System;
+using Penneo;
+using System.Collections.Generic;
+using System.Linq;
+using System.Diagnostics;
+
+namespace Pennneo
+{
+    public class LinkFolders
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length != 4) {
+                Console.WriteLine("Parameters required: endpoint, key, secret, customerId");
+                Environment.Exit(-1);
+            }
+
+            string endpoint   = args[0];
+            string key        = args[1];
+            string secret     = args[2];
+            string customerId = args[3];
+
+            PenneoConnector.Initialize(key, secret, endpoint);
+            PenneoConnector.SetLogger(new Logger());
+            run(customerId);
+        }
+
+        public static void run(String customerId)
+        {
+            RestConnector connector = new RestConnector();
+            var response = connector.InvokeRequest("/customers/" + customerId + "/users");
+
+            // Console.WriteLine(response.StatusCode);
+            Console.WriteLine(response.Content);
+        }
+    }
+
+    internal class Logger : ILogger
+    {
+        public void Log(string message, LogSeverity severity)
+        {
+            // Debug.WriteLine(severity + ": " + message);
+        }
+    }
+}
