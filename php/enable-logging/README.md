b73f61d60f23630d2b9bf9dee4faa20056bb3f0e
Ahmad N. Raja <ar@penneo.com>
2017-06-14 15:44:02 +0200

    Example for enabling logging for the php sdk

---
+++ php/enable-logging/README.md
@@ -0,0 +1,12 @@
+# Create a Signing Request
+
+Works with php sdk >= 1.6.0
+
+## Run
+
+```
+php run.php \
+    --endpoint="https://sandbox.penneo.com/api/v1/" \
+    --key=key \
+    --secret=secret
+```
---
+++ php/enable-logging/run.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * This script shows how to enable logging for the php sdk
+ *
+ * Works with version >= 1.6.0
+ *
+ * Usage:
+ * php run.php \
+ *  --endpoint="https://sandbox.penneo.com/api/v1/" \
+ *  --key=key \
+ *  --secret=secret
+ */
+
+namespace Penneo\SDK;
+
+require(__DIR__ . '/vendor/autoload.php');
+
+use Psr\Log\NullLogger;
+use Psr\Log\AbstractLogger;
+
+// Inputs
+//
+$opts = getopt(
+    "",
+    [
+        // Required
+        //
+        'endpoint:',
+        'key:',
+        'secret:',
+    ]
+);
+
+$endpoint   = $opts['endpoint'];
+$key        = $opts['key'];
+$secret     = $opts['secret'];
+
+// Create a custom logger
+
+class MyLogger extends AbstractLogger
+{
+    public function log($level, $message, array $context = array())
+    {
+        $message = (string) $message;
+        echo "$level : $message : " . print_r($context, true);
+    }
+}
+
+// Initialize the connection to the API
+//
+ApiConnector::initialize($key, $secret, $endpoint, null, ['Accept' => 'application/json']);
+ApiConnector::setLogger(new MyLogger());
+
+// Request and response bodies will be logged when calling a function that
+// generates an API request:
+//
+CaseFile::find(999999999);
