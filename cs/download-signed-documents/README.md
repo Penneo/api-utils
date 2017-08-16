2667645ebebb33bbe04ab567054bbdd1f4a4a2e2
Ahmad N. Raja <ar@penneo.com>
2017-08-16 14:13:46 +0200

    Example for getting documents and downloading documents

---
+++ cs/download-signed-documents/README.md
@@ -0,0 +1,5 @@
+# Get filtered Case Files
+
+## Requirements
+
+This example runs on Penneo .NET SDK version 1.3.0 and above.
---
+++ cs/download-signed-documents/run.cs
@@ -0,0 +1,62 @@
+using System;
+using Penneo;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Diagnostics;
+
+namespace Penneo
+{
+    public class DownloadSignedDocuments
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length != 4) {
+                Console.WriteLine("Parameters required: endpoint, key, secret, case-file-id");
+                Environment.Exit(-1);
+            }
+
+            string endpoint = args[0];
+            string key      = args[1];
+            string secret   = args[2];
+
+            int caseFileId  = System.Convert.ToInt32(args[3]);
+
+            PenneoConnector.Initialize(key, secret, endpoint);
+            PenneoConnector.SetLogger(new Logger());
+            run(caseFileId);
+        }
+
+        public static void run(int caseFileId)
+        {
+            var caseFile = Query.Find<CaseFile>(caseFileId);
+
+            // Check empty response
+            if (caseFile == null) {
+                Console.WriteLine("Nothing found");
+                return;
+            }
+
+            Console.WriteLine("Case File Id: " + caseFile.Id);
+
+            var documents = caseFile.GetDocuments();
+
+            foreach (Document document in documents)
+            {
+                Console.WriteLine("- " + document.Id + " : " + document.Title + "(" + document.GetStatus() + ")");
+                File.WriteAllBytes("downloaded-" + document.Id + ".pdf", document.GetPdf());
+            }
+        }
+    }
+
+    internal class Logger : ILogger
+    {
+        public void Log(string message, LogSeverity severity)
+        {
+            if (severity >= LogSeverity.Error)
+            {
+                Debug.WriteLine(severity + ": " + message);
+            }
+        }
+    }
+}
